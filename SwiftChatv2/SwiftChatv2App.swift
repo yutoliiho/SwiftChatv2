7e56da846f82f3bd80b8cfd06bbc5be769176c39
Tong Yu <yutongsf@gmail.com>
2023-05-09 00:20:45 -0700

    Initial Commit

---
+++ SwiftChatv2/SwiftChatv2App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftChatv2App.swift
+//  SwiftChatv2
+//
+//  Created by Yu, Tong on 5/9/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftChatv2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
